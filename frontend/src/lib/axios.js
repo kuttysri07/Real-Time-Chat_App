aeeb92d3bcb1a1d992d112b23f9dd497d35f140d
Sriram <sriram29072002@gmail.com>
2024-12-11 11:42:09 +0530

    for err

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:8000" : "/",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:8000" : "/api",
   withCredentials: true,
 });
